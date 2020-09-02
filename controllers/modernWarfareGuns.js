ead5ad6ac0f959ae3ccf6b2c739215187932ac45
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-01 22:22:06 -0700

    [get and post] set up get and post routes with proper catch errors

--- controllers/modernWarfareGuns.js
+++ controllers/modernWarfareGuns.js
@@ -4,5 +4,31 @@ const router = require('express').Router()
 // import models
 const db = require('../models')
 
+router.get('/', (req, res) => {
+    db.AssaultRifle.find()
+    .then(foundWeapons => {
+        res.send(foundWeapons)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(503).send({message: 'Database is sleeping zzzzz....'})
+    })
+})
+
+router.post('/', (req, res) => {
+    db.AssaultRifle.create(req.body)
+    .then(createWeapon=> {
+        res.status(201).send(createWeapon)
+    })
+    .catch(err => {
+        if(err.name == 'ValidationError') {
+            res.status(406).send({message: 'Validation Error'})
+        } else {
+            res.status(504).send({message: 'Database or server error.'})
+        }
+    })
+})
+
+
 
 module.exports = router
