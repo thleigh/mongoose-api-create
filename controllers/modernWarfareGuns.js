accee195d04adee1d097bddaa57dd845c8ac7e40
Tanner Leigh <tannerhleigh@gmail.com>
2020-09-01 22:38:25 -0700

    [CRUD] Finish crud

--- controllers/modernWarfareGuns.js
+++ controllers/modernWarfareGuns.js
@@ -29,6 +29,47 @@ router.post('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    db.AssaultRifle.findById(req.params.id)
+    .then(foundWeapon=>{
+        if(foundWeapon){
+            res.send(foundWeapon)
+        } else {
+            res.status(404).send({message: 'Resource not located.'})
+        }
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(503).send({message: 'Service Unavailable'})
+    })
+})
 
+router.put('/:id', (req, res) => {
+    db.AssaultRifle.findOneAndUpdate({
+        _id: req.params.id
+    },
+    req.body,
+    {
+        new: true
+    })
+    .then(updatedWeapon=>{
+        res.send(updatedWeapon)
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(503).send({message: 'Server Error'})
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    db.AssaultRifle.findByIdAndDelete(req.params.id)
+    .then(()=> {
+        res.status(204).send()
+    })
+    .catch(err => {
+        console.log(err)
+        res.send(503).send({message: 'Server Error'})
+    })
+})
 
 module.exports = router
